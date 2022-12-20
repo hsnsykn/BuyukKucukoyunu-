6a7d119965a914cb80560d15018b6c1a5c437539
hsnsykn <100951871+hsnsykn@users.noreply.github.com>
2022-12-20 11:26:42 +0300

    Büyük Küçük Oyunu

---
+++ BüyükKüçükoyunu.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JOptionPane;
+
+public class BüyükKüçükoyunu {
+    public static void main(String[] args) {
+   boolean oyunDevam = true;
+   int eskiSayı =0,yeniSayı,puan = 0;
+   Object[]etiket = {"küçük","büyük","çıkış"};
+   
+        do {            
+         yeniSayı = (int)(Math.random()*10)+1;
+         int cevap = JOptionPane.showOptionDialog(
+         null,//kime bağlı
+         "eski = "+ eskiSayı +       
+         
+                
+         )
+        } while (oyunDevam);
+   
+}
+}
